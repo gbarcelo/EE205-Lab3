00d7178397c4e2ad3a732557b52255b2f9e57094
gbarcelo <gbarcelo@users.noreply.github.com>
2018-02-02 23:27:39 -1000

    Initial commit for new branch feat_test-Vector
     - Merged MakeFileEdits1 to get completed Task 1
     - Edited makefile "Clean" to ignore errors and continue cleaning
     - Implemented Vector.cpp constructors and destructors, awaiting testing

--- problem/src/Vector.cpp
+++ problem/src/Vector.cpp
@@ -2,3 +2,24 @@
 
 #include <utility>
 
+// Con-/De-structors:
+// - Default constructor
+Vector::Vector() : arr(NULL), length(0), capacity(0) { }
+
+// - Deep Copy constructor
+Vector::Vector(const Vector& other) : arr(new int(*other.arr)), length(other.length), capacity(other.capacity) { }
+
+// - Steal Copy constructor
+Vector::Vector(Vector&& other) : arr(other.arr), length(other.length), capacity(other.capacity) {
+  other.arr = nullptr;
+  other.length = 0;
+  other.capacity = 0; // Question: Necessary?
+}
+
+// Deallocate memory
+Vector::~Vector() { }
+
+// Private Method:
+void Vector::double_capacity() {
+  // Double capacity of current vector
+}
